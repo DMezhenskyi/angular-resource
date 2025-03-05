3862f628822c7cc4ca68254c87866926b775424b
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2025-03-05 12:26:18 +0100

    ready initial state

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, resource, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { API_URL } from './config';
 import { User } from './model';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
@@ -11,19 +11,13 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
       <legend>Users Search</legend>
       <input (input)="query.set($any($event.target).value)" type="search" placeholder="Search...">
     </fieldset>
-    @if (users.isLoading()) {
-      <mat-progress-bar mode="query" />
-    }
-    @if (users.error()) {
-      <div class="error">{{users.error()}}</div>
-    }
     <section class="actions">
-      <button (click)="users.reload()">Reload</button>
-      <button (click)="addUser()">Add User</button>
-      <button (click)="users.set([])">Clear</button>
+      <button>Reload</button>
+      <button>Add User</button>
+      <button>Clear</button>
     </section>
     <ul>
-      @for (user of users.value(); track user.id) {
+      @for (user of users(); track user.id) {
         <li>{{ user.name }}</li>
       } @empty {
         <li class="no-data">Nothing to show</li>
@@ -33,21 +27,13 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 })
 export class UserSearchComponent {
   query = signal('');
-  users = resource<User[], { query: string }>({
-    request: () => ({ query: this.query() }),
-    loader: async ({request, abortSignal}) => {
-      const users = await fetch(`${API_URL}?name_like=^${request.query}`, {
-        signal: abortSignal
-      });
-      if (!users.ok) throw Error(`Could not fetch...`)
-      return await users.json();
-    }
-  });
-  constructor() {}
+
+  users = signal<User[]>([]);
+  
   addUser() {
     const user = { id: 123, name: "Dmytro Mezhenskyi" };
-    this.users.update(
-      users => users ? [user, ...users] : [user]
-    )
+    // this.users.update(
+    //   users => users ? [user, ...users] : [user]
+    // )
   }
 }
