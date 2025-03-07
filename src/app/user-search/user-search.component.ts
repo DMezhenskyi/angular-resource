f4e5532ac52537ad26cf1830a0f17222f3af4456
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2025-03-07 11:48:55 +0100

    done

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -2,6 +2,15 @@ import { Component, signal } from '@angular/core';
 import { API_URL } from './config';
 import { User } from './model';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { httpResource } from '@angular/common/http';
+import {z as zod} from 'zod'; 
+
+const UsersSchema = zod.array(
+  zod.object({
+    id: zod.number(),
+    name: zod.string()
+  })
+)
 
 @Component({
   selector: 'app-user-search',
@@ -11,13 +20,19 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
       <legend>Users Search</legend>
       <input (input)="query.set($any($event.target).value)" type="search" placeholder="Search...">
     </fieldset>
+    @if (users.isLoading()) {
+      <mat-progress-bar mode="query" />
+    }
+    @if(users.error()) {
+      <div class="error">Couldn't fetch data...</div>
+    }
     <section class="actions">
-      <button>Reload</button>
-      <button>Add User</button>
-      <button>Clear</button>
+      <button (click)="users.reload()">Reload</button>
+      <button (click)="addUser()">Add User</button>
+      <button (click)="users.set([])">Clear</button>
     </section>
     <ul>
-      @for (user of users(); track user.id) {
+      @for (user of users.value(); track user.id) {
         <li>{{ user.name }}</li>
       } @empty {
         <li class="no-data">Nothing to show</li>
@@ -28,12 +43,19 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 export class UserSearchComponent {
   query = signal('');
 
-  users = signal<User[]>([]);
+  users = httpResource(
+    () => ({
+      url: `${API_URL}?name_like=^${this.query()}`,    }),
+    {
+      defaultValue: [],
+      parse: UsersSchema.parse
+    }
+  );
   
   addUser() {
     const user = { id: 123, name: "Dmytro Mezhenskyi" };
-    // this.users.update(
-    //   users => users ? [user, ...users] : [user]
-    // )
+    this.users.update(
+      users => users ? [user, ...users] : [user]
+    )
   }
 }
