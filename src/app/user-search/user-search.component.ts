c215fd5e19751992abb37f5e0a0b76104833c0d3
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2026-06-11 09:42:20 +0200

    implemented

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, linkedSignal, Resource, resourceFromSnapshots, ResourceSnapshot, signal } from '@angular/core';
 import { API_URL } from './config';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { httpResource } from '@angular/common/http';
@@ -12,14 +12,14 @@ import { User } from './model';
       <legend>Users Search</legend>
       <input (input)="query.set($event.target.value)" type="search" placeholder="Search...">
     </fieldset>
-    @if (userResource.isLoading()) {
+    @if (users.isLoading()) {
       <mat-progress-bar mode="query" />
     }
-    @if(userResource.error()) {
+    @if(users.error()) {
       <div class="error">Couldn't fetch data...</div>
     }
     <ul>
-      @for (user of userResource.value(); track user.id) {
+      @for (user of users.value(); track user.id) {
         <li>{{ user.name }}</li>
       } @empty {
         <li class="no-data">Nothing to show</li>
@@ -30,9 +30,19 @@ import { User } from './model';
 export class UserSearchComponent {
   protected readonly query = signal('');
 
-  protected readonly userResource = httpResource<User[]>(
-    () => `${API_URL}${this.query()}`
+  private readonly userResource = httpResource<User[]>(
+    () => `${API_URL}${this.query()}`,
+    { defaultValue: [] }
   );
+
+  protected readonly users = withPreviousValue(
+    withSorting(
+      this.userResource,
+      (a, b) => a.id - b.id
+    )
+  )
+
+  constructor() {}
   
   addUser() {
     const user = { id: 123, name: "Dmytro Mezhenskyi" };
@@ -40,4 +50,34 @@ export class UserSearchComponent {
       users => users ? [user, ...users] : [user]
     )
   }
+}
+function withPreviousValue<T>(resource: Resource<T>): Resource<T> {
+  const derivedResource = linkedSignal({
+    source: resource.snapshot,
+    computation: (snap, prev): ResourceSnapshot<T> => {
+      if (snap.status === 'loading' && prev && prev.value.status !== 'error') {
+        return { ...snap, value: prev.value.value };
+      }
+
+      return snap;
+    }
+  });
+  return resourceFromSnapshots(derivedResource);
+}
+
+function withSorting<T>(
+  resource: Resource<T[]>,
+  sortFn: (a: T, b: T) => number
+): Resource<T[]> {
+  const derivedResource = computed((): ResourceSnapshot<T[]> => {
+    const snap = resource.snapshot();
+    
+    if (snap.status != 'error' && snap.value) {
+      return { ...snap, value: [...snap.value].sort(sortFn) };
+    }
+    
+    return snap;
+  });
+  
+  return resourceFromSnapshots(derivedResource);
 }
